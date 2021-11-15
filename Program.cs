a069e484061d4edf6e5689e0a690ca856a0eef83
GEORGIANA-LAURA NEGREA <georgiana.laura.negrea@stud.ubbcluj.ro>
2021-11-15 13:09:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Negrea_Georgiana_Lab2.Data;
+
+namespace Negrea_Georgiana_Lab2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<LibraryContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
+        }
+    }
+}
